2737f4c4a0f38cf91936539968150a157a84ba8f
rrifka <rifkatasyaramadhani18@gmail.com>
2024-08-22 11:10:14 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Manakah dari berikut ini yang merupakan tag HTML untuk membuat paragraf?",
+        answer : "C. p",
+        options: [
+            "A. div",
+            "B. span",
+            "C. p",
+            "D. section"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Dalam CSS, bagaimana cara mengatur warna teks menjadi merah?",
+        answer : "A. color: red;",
+        options: [
+            "A. color: red;",
+            "B. background-color: red;",
+            "C. text-color: red;",
+            "D. font-color: red;"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Manakah dari berikut ini yang digunakan untuk mendeklarasikan variabel di JavaScript?",
+        answer : "D. Semua jawaban benar",
+        options: [
+            "A. var",
+            "B. let",
+            "C. const",
+            "D. Semua jawaban benar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Di dalam HTML, atribut href digunakan untuk",
+        answer : "B. Menambahkan tautan ke halaman lain",
+        options: [
+            "A. Mengatur ukuran gambar",
+            "B. Menambahkan tautan ke halaman lain",
+            "C. Mengatur teks tebal",
+            "D. Menambahkan judul halaman"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Di dalam CSS, properti apa yang digunakan untuk mengatur ukuran teks?",
+        answer : "C. font-size",
+        options: [
+            "A. font-style",
+            "B. text-size",
+            "C. font-size",
+            "D. text-style"
+        ]
+    }
+];
